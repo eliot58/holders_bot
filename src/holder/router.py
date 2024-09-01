9b0b73593e638f7818484c327a9011989641d2fc
Aiomedin Chorobaev <eliothab58@gmail.com>
2024-09-02 00:32:45 +0600

    fix

--- src/holder/router.py
+++ src/holder/router.py
@@ -15,6 +15,7 @@ router = APIRouter(
 
 @router.post("/check")
 async def check(data: WalletSchema):
+    print(data.initData)
     userId = await get_userId(data.initData)
 
     address = data.wallet["account"]["address"]
