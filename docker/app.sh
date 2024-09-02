ec78b131ac8cf202d7252e8d11a9e6bbe5ed41b5
Aiomedin Chorobaev <eliothab58@gmail.com>
2024-09-02 17:39:32 +0600

    fix

--- docker/app.sh
+++ docker/app.sh
@@ -2,4 +2,4 @@
 
 aerich upgrade
 
-gunicorn main:app --workers 3 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+gunicorn main:app --workers 3 --timeout 120 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
